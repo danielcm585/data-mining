34a76d068c0aaa80629b297cce39a71ade0b8d0d
Daniel Christian Mandolang <74773920+danielcm585@users.noreply.github.com>
2024-02-29 23:08:44 +0700

    fixed no1

--- assignment/assignment05/no_1.py
+++ assignment/assignment05/no_1.py
@@ -6,7 +6,9 @@ graph = Graph('/Users/danielcm585/Developer/data-mining/assignment/assignment05/
 degree_centrality = graph.get_degree_centrality()
 for i in range(len(graph.nodes)):
     node = graph.nodes[i]
-    print(f"{node}'s degree centrality is {degree_centrality[node.id]} and these are the neighbors:")
+    print(node)
+    print(f"> degree centrality: {degree_centrality[node.id]}")
+    print(f"> neighbors:")
     for neighbor in node.neighbors:
-        print(f"- {neighbor}")
+        print(f"  - {neighbor}")
     
