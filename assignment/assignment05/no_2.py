bf346eccad959bee65c6ac52c0f9c0b32cd0ee10
Daniel Christian Mandolang <74773920+danielcm585@users.noreply.github.com>
2024-03-01 13:15:06 +0700

    fixed no2

--- assignment/assignment05/no_2.py
+++ assignment/assignment05/no_2.py
@@ -7,10 +7,10 @@ graph = Graph(
 )
     
 in_degree_centrality, out_degree_centrality = graph.get_degree_centrality()
-print(f"{'Node':10} {'In Degree':12} {'Out Degree':12} {'In Degree Centrality':22} {'Out Degree Centrality':22}")
+print(f"{'Node':10} {'In Degree Centrality':22} {'Out Degree Centrality':22}")
 for i in range(len(graph.nodes)):
     node = graph.nodes[i]
-    print("{:10} {:<12} {:<12} {:<22} {:<22}".format(
+    print("{:10} {:<22} {:<22}".format(
         node.name,
         node.in_degree,
         node.out_degree,
